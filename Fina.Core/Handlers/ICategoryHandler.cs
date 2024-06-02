1dd9421223ac9c27f4c408c28fed674be188ccee
lucasg-meneses <lucasgmeneses.dev@gmail.com>
2024-06-02 13:45:57 -0400

    add handler interface

---
+++ Fina.Core/Handlers/ICategoryHandler.cs
@@ -0,0 +1,15 @@
+﻿using Fina.Core.Models;
+using Fina.Core.Request.Categories;
+using Fina.Core.Response;
+
+namespace Fina.Core.Handlers
+{
+    public interface ICategoryHandler
+    {
+        Task<Response<Category?>> CreateAsync(CreateCategoryRequest request);
+        Task<Response<Category?>> DelateAsync(DeleteCategoryRequest request);
+        Task<Response<Category?>> UpdateAsync(UpdatedCategoryRequest request);
+        Task<Response<Category?>> GetByIdAsync(GetCategoryByIdRequest request);
+        Task<PagedResponse<List<Category>?>> GetAllAsync(GetAllCategoryRequest request);
+    }
+}
---
+++ Fina.Core/Handlers/ITransactionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Transactions;
+using Fina.Core.Request.Transactions;
+using Fina.Core.Response;
+
+namespace Fina.Core.Handlers
+{
+
+    public interface ITransactionHandler
+    {
+        Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest request);
+        Task<Response<Transaction?>> DelateAsync(DeleteTransactionRequest request);
+        Task<Response<Transaction?>> UpdateAsync(UpdatedTransactionRequest request);
+        Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request);
+        Task<PagedResponse<List<Transaction>?>> GetAllAsync(GetTransactionByPeriodRequest request);
+    }
+}
